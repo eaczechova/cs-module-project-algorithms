9f81effaf046248f953d0c4b9b0ce15214ccb68f
eaczechova <ewa.czech@hotmail.com>
2020-08-12 14:45:00 +0200

    add solution for single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,21 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    while len(arr) > 0:
+        # select a number and remove it from the list
+        number = arr.pop(0)
 
-    pass
+        # if the number is not in the list, we return the single number
+        if number not in arr:
+            return number
 
+        # else we remove it by the value
+        else:
+            arr.remove(number)
+    
+        
+        
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
